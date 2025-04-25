0d6780c0918ca2ae23d16e5381a453a92c5490b5
鹿先森 <1173381395@qq.com>
2025-04-25 11:29:09 +0800

    自动创建目录

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,12 @@
 # coding=utf-8
 # @Time     :2025/4/25 09:10
 # @Author   :luye
+import os
 
 LOG_PATH = 'logs'
 DB_PATH = 'db'
 RES_PATH = 'resources'
+
+for folder in [LOG_PATH, DB_PATH]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
